a445a297b842499f97ff9b76a6e13710351a9286
Nambinintsoa <nambinintsoa@gmail.com>
2025-07-31 10:04:06 +0300

    Ajout de la route  exception_handeler pour l'évaluation

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.responses import HTMLResponse, PlainTextResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from pydantic import BaseModel
+from datetime import datetime
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from typing import List
 
 app = FastAPI()
+security = HTTPBasic()
+
+class Post(BaseModel):
+    author: str
+    title: str
+    content: str
+    creation_datetime: datetime
 
 @app.get("/ping", response_class=PlainTextResponse)
 async def ping():
@@ -11,3 +23,10 @@ async def ping():
 @app.get("/home")
 async def home():
     return {"message": "Welcome to the home page"}
+
+
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request, exc):
+    if exc.status_code == 404:
+        return HTMLResponse(content="404 NOT FOUND", status_code=404)
+    return await exc
